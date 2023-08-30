5cc5b5c89c191292601b2245d01c88653c28a08f
James Burke (IDAM) <james.burke@hmcts.net>
2023-08-30 12:16:16 +0100

    smoke

---
+++ src/smokeTest/java/uk/gov/hmcts/idam/userprofilebridge/controllers/SpringSmokeTest.java
@@ -0,0 +1,14 @@
+package uk.gov.hmcts.idam.userprofilebridge.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SpringSmokeTest {
+
+    @Test
+    public void contextLoads() throws Exception {
+        assert true;
+    }
+
+}
