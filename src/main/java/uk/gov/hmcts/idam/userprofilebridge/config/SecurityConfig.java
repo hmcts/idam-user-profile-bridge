d8e8a1d203ceee5dc1af486b37fdf3bfd5c6108c
James Burke (IDAM) <james.burke@hmcts.net>
2023-08-31 13:51:15 +0100

    security

--- src/main/java/uk/gov/hmcts/idam/userprofilebridge/config/SecurityConfig.java
+++ src/main/java/uk/gov/hmcts/idam/userprofilebridge/config/SecurityConfig.java
@@ -19,6 +19,8 @@ public class SecurityConfig {
         http
             .csrf(AbstractHttpConfigurer::disable)
             .authorizeHttpRequests((authz) -> authz
+                .requestMatchers("/info").permitAll()
+                .requestMatchers("/health").permitAll()
                 .anyRequest().authenticated()
             )
             .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
