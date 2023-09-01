b1e17f7904f6d2d5d464840fd77f06821e82e95c
James Burke (IDAM) <james.burke@hmcts.net>
2023-09-01 15:29:56 +0100

    remove debug

--- src/main/java/uk/gov/hmcts/idam/userprofilebridge/controllers/UserProfileController.java
+++ src/main/java/uk/gov/hmcts/idam/userprofilebridge/controllers/UserProfileController.java
@@ -3,7 +3,6 @@ package uk.gov.hmcts.idam.userprofilebridge.controllers;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -22,15 +21,6 @@ import uk.gov.hmcts.idam.userprofilebridge.service.UserProfileService;
 @Slf4j
 public class UserProfileController {
 
-    @Value("${spring.security.oauth2.client.registration.idam-user-profile-bridge.client-secret:nothing}")
-    private String idamSecretValue;
-
-    @Value("${spring.security.oauth2.client.registration.rd-userprofile-api.client-secret:nothing}")
-    private String rdSecretValue;
-
-    @Value("${app-insights-connection-string:nothing}")
-    private String insightConnString;
-
     private final UserProfileService userProfileService;
 
     public UserProfileController(UserProfileService userProfileService) {
@@ -43,9 +33,6 @@ public class UserProfileController {
     @SecurityRequirement(name = "bearerAuth")
     public User getUserById(@AuthenticationPrincipal @Parameter(hidden = true) Jwt principal,
                             @PathVariable String userId) {
-        log.info("Idam client secret is: {}", idamSecretValue);
-        log.info("Rd client secret is: {}", rdSecretValue);
-        log.info("insight conn string is: {}", insightConnString);
         return userProfileService.getUserById(userId);
     }
 
