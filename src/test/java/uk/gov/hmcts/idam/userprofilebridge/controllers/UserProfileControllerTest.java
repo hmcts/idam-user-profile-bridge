dd5f636f65ac9f9afd3a5a68f112b9e0db15219e
James Burke (IDAM) <james.burke@hmcts.net>
2023-09-05 09:29:16 +0100

    tests

---
+++ src/test/java/uk/gov/hmcts/idam/userprofilebridge/controllers/UserProfileControllerTest.java
@@ -0,0 +1,75 @@
+package uk.gov.hmcts.idam.userprofilebridge.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.test.web.servlet.MockMvc;
+import uk.gov.hmcts.idam.userprofilebridge.service.UserProfileService;
+
+import static org.mockito.Mockito.verify;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.jwt;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest
+class UserProfileControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    UserProfileService userProfileService;
+
+    @Test
+    public void getUserById() throws Exception {
+        mockMvc.perform(
+                get("/idam/api/v2/users/1234")
+                    .with(jwt()
+                              .authorities(new SimpleGrantedAuthority("SCOPE_profile"))
+                              .jwt(token -> token.claim("aud", "test-client").build())))
+            .andExpect(status().isOk());
+
+        verify(userProfileService).getUserById("1234");
+    }
+
+    @Test
+    public void getUserProfileById() throws Exception {
+        mockMvc.perform(
+                get("/rd/api/v1/userprofile/1234")
+                    .with(jwt()
+                              .authorities(new SimpleGrantedAuthority("SCOPE_profile"))
+                              .jwt(token -> token.claim("aud", "test-client").build())))
+            .andExpect(status().isOk());
+
+        verify(userProfileService).getUserProfileById("1234");
+    }
+
+    @Test
+    public void getCaseworkerProfileById() throws Exception {
+        mockMvc.perform(
+                get("/rd/case-worker/profile/search-by-id/1234")
+                    .with(jwt()
+                              .authorities(new SimpleGrantedAuthority("SCOPE_profile"))
+                              .jwt(token -> token.claim("aud", "test-client").build())))
+            .andExpect(status().isOk());
+
+        verify(userProfileService).getCaseWorkerProfileById("1234");
+    }
+
+    @Test
+    public void syncIdamUser() throws Exception {
+        mockMvc.perform(
+            put("/bridge/user/1234")
+                .with(jwt()
+                          .authorities(new SimpleGrantedAuthority("SCOPE_profile"))
+                          .jwt(token -> token.claim("aud", "test-client").build())))
+            .andExpect(status().isOk());
+
+        verify(userProfileService).requestSyncIdamUser("1234");
+
+    }
+
+}
