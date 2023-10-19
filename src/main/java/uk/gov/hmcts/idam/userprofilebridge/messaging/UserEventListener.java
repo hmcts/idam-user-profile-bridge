b916f7c7ba5af376e0df9ba25e58cf71b4d9c9d2
James Burke (IDAM) <james.burke@hmcts.net>
2023-10-19 17:19:23 +0100

    initial

--- src/main/java/uk/gov/hmcts/idam/userprofilebridge/messaging/UserEventListener.java
+++ src/main/java/uk/gov/hmcts/idam/userprofilebridge/messaging/UserEventListener.java
@@ -1,10 +1,12 @@
 package uk.gov.hmcts.idam.userprofilebridge.messaging;
 
+import io.opentelemetry.api.trace.Span;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.idam.userprofilebridge.messaging.model.UserEvent;
 import uk.gov.hmcts.idam.userprofilebridge.service.UserEventService;
+import uk.gov.hmcts.idam.userprofilebridge.trace.TraceAttribute;
 
 @Slf4j
 @Component
@@ -19,14 +21,14 @@ public class UserEventListener {
     @JmsListener(destination = "${idam.messaging.subscription.modify-user}", containerFactory =
         "jmsListenerContainerFactory")
     public void receiveModifyUserEvent(UserEvent event) {
-        log.info("Received modify user {}", event.getUser().getId());
+        Span.current().setAttribute(TraceAttribute.USER_ID, event.getUser().getId());
         userEventService.handleModifyUserEvent(event);
     }
 
     @JmsListener(destination = "${idam.messaging.subscription.add-user}", containerFactory =
         "jmsListenerContainerFactory")
     public void receiveAddUserEvent(UserEvent event) {
-        log.info("Received add user {}", event.getUser().getId());
+        Span.current().setAttribute(TraceAttribute.USER_ID, event.getUser().getId());
         userEventService.handleAddUserEvent(event);
     }
 
