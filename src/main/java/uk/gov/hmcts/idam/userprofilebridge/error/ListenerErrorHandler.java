0457a99bfa5a04543b4b615ec8c7e73f4bd46c24
James Burke (IDAM) <james.burke@hmcts.net>
2023-10-24 17:26:56 +0100

    capture error body

--- src/main/java/uk/gov/hmcts/idam/userprofilebridge/error/ListenerErrorHandler.java
+++ src/main/java/uk/gov/hmcts/idam/userprofilebridge/error/ListenerErrorHandler.java
@@ -1,21 +1,28 @@
 package uk.gov.hmcts.idam.userprofilebridge.error;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
 import org.springframework.util.ErrorHandler;
 import org.springframework.web.client.HttpStatusCodeException;
+import uk.gov.hmcts.cft.idam.api.v2.common.error.SpringWebClientHelper;
+
+import java.util.List;
+
+import static uk.gov.hmcts.cft.idam.api.v2.common.error.SpringWebClientHelper.convertJsonToMap;
+import static uk.gov.hmcts.cft.idam.api.v2.common.error.SpringWebClientHelper.extractMessagesFromMap;
 
 @Component
 @Slf4j
 public class ListenerErrorHandler implements ErrorHandler {
     @Override
     public void handleError(Throwable t) {
-        if (t.getCause() != null && t.getCause() instanceof HttpStatusCodeException) {
-            log.debug(
-                "Listener led to HttpStatusCodeException exception, {}; {}",
-                ((HttpStatusCodeException) t.getCause()).getStatusCode(),
-                t.getCause().getMessage()
-            );
+        if (t.getCause() != null && t.getCause() instanceof HttpStatusCodeException hsce) {
+            String responseBody = hsce.getResponseBodyAsString();
+            if (StringUtils.isNotEmpty(responseBody)) {
+                log.warn("Listener led to http exception {};{};{}", hsce.getStatusCode(), hsce.getMessage(), responseBody);
+            }
         } else {
             log.error("Listener led to exception", t);
         }
--- src/test/java/uk/gov/hmcts/idam/userprofilebridge/error/ListenerErrorHandlerTest.java
+++ src/test/java/uk/gov/hmcts/idam/userprofilebridge/error/ListenerErrorHandlerTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.jms.listener.adapter.ListenerExecutionFailedException;
 import uk.gov.hmcts.cft.idam.api.v2.common.error.SpringWebClientHelper;
@@ -17,10 +18,19 @@ class ListenerErrorHandlerTest {
     @Test
     public void testHandleError() {
         // coverage only
+        underTest.handleError(new ListenerExecutionFailedException(
+            "test-fail",
+            SpringWebClientHelper
+                .exception(HttpStatus.I_AM_A_TEAPOT,
+                           "test-message",
+                           new HttpHeaders(),
+                           "test-body".getBytes()
+                )
+                .orElse(new RuntimeException())
+        ));
         underTest.handleError(new ListenerExecutionFailedException("test-fail",
-                                                                   SpringWebClientHelper.exception(
-                                                                       HttpStatus.NOT_FOUND,
-                                                                       new RuntimeException()
+                                                                   SpringWebClientHelper.exception(HttpStatus.NOT_FOUND,
+                                                                                                   new RuntimeException()
                                                                    )
         ));
         underTest.handleError(new RuntimeException());
