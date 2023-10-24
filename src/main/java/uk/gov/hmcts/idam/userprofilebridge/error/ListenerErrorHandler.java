21abb8cac4788c5a2934e2e5574df0f862efac50
James Burke (IDAM) <james.burke@hmcts.net>
2023-10-24 17:32:45 +0100

    capture error body

--- src/main/java/uk/gov/hmcts/idam/userprofilebridge/error/ListenerErrorHandler.java
+++ src/main/java/uk/gov/hmcts/idam/userprofilebridge/error/ListenerErrorHandler.java
@@ -3,15 +3,8 @@ package uk.gov.hmcts.idam.userprofilebridge.error;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
-import org.springframework.util.CollectionUtils;
 import org.springframework.util.ErrorHandler;
 import org.springframework.web.client.HttpStatusCodeException;
-import uk.gov.hmcts.cft.idam.api.v2.common.error.SpringWebClientHelper;
-
-import java.util.List;
-
-import static uk.gov.hmcts.cft.idam.api.v2.common.error.SpringWebClientHelper.convertJsonToMap;
-import static uk.gov.hmcts.cft.idam.api.v2.common.error.SpringWebClientHelper.extractMessagesFromMap;
 
 @Component
 @Slf4j
@@ -21,7 +14,12 @@ public class ListenerErrorHandler implements ErrorHandler {
         if (t.getCause() != null && t.getCause() instanceof HttpStatusCodeException hsce) {
             String responseBody = hsce.getResponseBodyAsString();
             if (StringUtils.isNotEmpty(responseBody)) {
-                log.warn("Listener led to http exception {};{};{}", hsce.getStatusCode(), hsce.getMessage(), responseBody);
+                log.warn(
+                    "Listener led to http exception {};{};{}",
+                    hsce.getStatusCode(),
+                    hsce.getMessage(),
+                    responseBody
+                );
             }
         } else {
             log.error("Listener led to exception", t);
