25af16cdc1dd8e2f6f665313aa4a43129aab49ae
James Burke (IDAM) <james.burke@hmcts.net>
2024-08-20 16:49:36 +0100

    merge master

--- src/main/java/uk/gov/hmcts/cft/idam/api/v2/common/IdamV2UserManagementApi.java
+++ src/main/java/uk/gov/hmcts/cft/idam/api/v2/common/IdamV2UserManagementApi.java
@@ -3,10 +3,9 @@ package uk.gov.hmcts.cft.idam.api.v2.common;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import uk.gov.hmcts.cft.idam.api.v2.common.auth.IdamClientCredentialsConfig;
 import uk.gov.hmcts.cft.idam.api.v2.common.model.User;
 
-@FeignClient(name = "idamv2usermanagement", url = "${idam.api.url}", configuration = IdamClientCredentialsConfig.class)
+@FeignClient(name = "idamv2usermanagement", url = "${idam.api.url}")
 public interface IdamV2UserManagementApi {
 
     @GetMapping("/api/v2/users/{userId}")
