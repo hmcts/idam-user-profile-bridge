246fb35e36a2708797fb4b2a10139f69607b2829
James Burke (IDAM) <james.burke@hmcts.net>
2025-11-18 10:43:30 +0000

    ignore unknown json attributes in events

--- src/main/java/uk/gov/hmcts/idam/userprofilebridge/messaging/model/UserEvent.java
+++ src/main/java/uk/gov/hmcts/idam/userprofilebridge/messaging/model/UserEvent.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.idam.userprofilebridge.messaging.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Getter;
 import lombok.Setter;
 import uk.gov.hmcts.cft.idam.api.v2.common.model.User;
@@ -8,6 +9,7 @@ import java.time.LocalDateTime;
 
 @Getter
 @Setter
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class UserEvent {
 
     EventType eventType;
